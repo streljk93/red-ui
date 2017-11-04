01ef805184583a5097cceb1346b73b72089a2fce
jk <strel.jk.93@gmail.com>
2017-11-05 00:00:32 +0300

    fixed for heroku

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 // require
 var express = require('express');
+var serveStatic = require('serve-static');
 
 // server app
 var app = express();
 
 // setting
-app.use(express.static('app'));
+app.use(serveStatic(__dirname + '/app/'));
+// app.use(express.static('app'));
 app.set('port', process.env.PORT || 3000);
 
 app.listen(app.get('port'), function() {
