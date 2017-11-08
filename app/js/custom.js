35cc9f9227ad21819896bad013cb86f37e8d2fbe
jk <strel.jk.93@gmail.com>
2017-11-09 00:14:17 +0300

    add media block

--- app/js/custom.js
+++ app/js/custom.js
@@ -1,67 +1,71 @@
-var start = new Date();
+(function() {
 
-timer(start, 30, function() {
-    console.log('ok');
-});
+  var start = new Date();
 
-$('.js-jcarousel__service').slick({
-  dots: false,
-  infinite: true,
-  speed: 300,
-  slidesToShow: 1,
-  slidesToScroll: 1,
-  centerMode: true,
-  adaptiveHeight: true,
-  arrows: false,
-});
+  timer(start, 30, function() {
+      console.log('ok');
+  });
 
-$('.js-jcarousel__promotions').slick({
-  dots: false,
-  infinite: true,
-  speed: 300,
-  slidesToShow: 3,
-  centerMode: true,
-  adaptiveHeight: true,
-  mobileFirst: true,
-  arrows: false,
-  responsive: [
-    {
-      breakpoint: 1024,
-      settings: {
-        slidesToShow: 3,
-        slidesToScroll: 3,
+  $('.js-jcarousel__service').slick({
+    dots: false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    centerMode: true,
+    adaptiveHeight: true,
+    arrows: false,
+  });
+
+  $('.js-jcarousel__promotions').slick({
+    dots: false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 3,
+    centerMode: true,
+    adaptiveHeight: true,
+    mobileFirst: true,
+    arrows: false,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 0,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
       }
-    },
-    {
-      breakpoint: 600,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 2
-      }
-    },
-    {
-      breakpoint: 480,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1
-      }
-    },
-    {
-      breakpoint: 0,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1
-      }
+    ]
+  });
+
+  $('.jcollapse .jcollapse__button').on('click', function() {
+    var aria = $(this)[0].attributes['aria-expanded'].value;
+    var show = (aria === 'true') ? false : true;
+    if(show) {
+      $(this).find('i')[0].className = 'fa fa-chevron-up';
+    } else {
+      $(this).find('i')[0].className = 'fa fa-chevron-down';
     }
-  ]
-});
+  });
 
-$('.jcollapse .jcollapse__button').on('click', function() {
-  var aria = $(this)[0].attributes['aria-expanded'].value;
-  var show = (aria === 'true') ? false : true;
-  if(show) {
-    $(this).find('i')[0].className = 'fa fa-chevron-up';
-  } else {
-    $(this).find('i')[0].className = 'fa fa-chevron-down';
-  }
-});
+}());
